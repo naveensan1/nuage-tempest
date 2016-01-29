b719721bf1579af0627b60dfad88538ba507d853
Nisar Mohamed <nisarmohamed.khan@alcatel-lucent.com>
2016-01-28 18:27:39 -0800

    New Setup files for Plugin Mode

--- setup.py
+++ setup.py
@@ -1,33 +1,6 @@
-from setuptools import setup
-# import versioneer
+import setuptools
 
-
-#def get_requirements():
-#    requirements = []
-#    with open('requirements.txt', 'r') as f:
-#        for line in f.readlines():
-#            requirements.append(line.strip())
-#    return requirements
-
-packages = [
-    'nuagetempest',
-]
-
-package_data = {}
-
-setup(
-    name='nuage-tempest',
-    version=0.1,
-    # version=versioneer.get_version(),
-    # cmdclass=versioneer.get_cmdclass(),
-    description='Nuage Tempest tests and libraries',
-    url='https://github.mv.usa.alcatel.com/pygash/nuage-tempest',
-    author='Nisar',
-    author_email='nisar@alcatel-lucent.com',
-    license='wtfpl',
-    packages=packages,
-    package_data=package_data,
-    include_package_data=False,
-    #install_requires=get_requirements(),
-    zip_safe=False
+setuptools.setup(
+    setup_requires=['pbr>=1.8'],
+    pbr=True
 )

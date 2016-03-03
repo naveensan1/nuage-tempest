8ad13f6b5f7cd7ad27ef2dbd804ebf644e96ffa3
aniketad <aniket_abhay.dandekar@alcatel-lucent.com>
2016-03-02 17:04:49 -0800

    Returning two folders for test discovery.
    Returning two folders for test discovery.

--- nuagetempest/plugin.py
+++ nuagetempest/plugin.py
@@ -17,9 +17,11 @@ class NuageTempestPlugin(plugins.TempestPlugin):
     def load_tests(self):
         base_path = os.path.split(os.path.dirname(
             os.path.abspath(__file__)))[0]
-        test_dir = "nuagetempest/tests"
-        full_test_dir = os.path.join(base_path, test_dir)
-        return full_test_dir, base_path
+        test_dirs = ["nuagetempest/tests", "nuagetempest/thirdparty"]
+        full_test_dirs = []
+        for test_dir in test_dirs:
+            full_test_dirs.append(os.path.join(base_path, test_dir))
+        return full_test_dirs, base_path
 
     def register_opts(self, conf):
         config.register_opt_group(
